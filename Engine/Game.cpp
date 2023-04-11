c0545f11c535d07cd06b7d3155d6a9e7aab27a78
Tom Garnand <tom.garnand@gmail.com>
2023-04-11 09:24:24 -0700

    finished 32bit & reverse compat, even better than chili

--- Engine/Game.cpp
+++ Engine/Game.cpp
@@ -25,7 +25,7 @@ Game::Game( MainWindow& wnd )
 	:
 	wnd( wnd ),
 	gfx( wnd ),
-	surf("dib.bmp")
+	surf("dibr.bmp")
 {
 }
 
--- Engine/Surface.cpp
+++ Engine/Surface.cpp
@@ -7,28 +7,50 @@
 Surface::Surface(const std::string& filename)
 {
 	std::ifstream file(filename, std::ios::binary);
+	assert(file);
 	BITMAPFILEHEADER bmFileHeader;
 	file.read(reinterpret_cast<char*>(&bmFileHeader),sizeof(bmFileHeader));
 
 	BITMAPINFOHEADER bmInfoHeader;
 	file.read(reinterpret_cast<char*>(&bmInfoHeader), sizeof(bmInfoHeader));
 
-	assert(bmInfoHeader.biBitCount == 24);
 	assert(bmInfoHeader.biCompression == BI_RGB);
 
 	width = bmInfoHeader.biWidth;
 	height = bmInfoHeader.biHeight;
 
+	int yStart;
+	int yEnd;
+	int dy;
+
+	if (height < 0)
+	{
+		height = -height;
+		yStart = 0;
+		yEnd = height;
+		dy = 1;
+		
+	}
+	else
+	{
+		yStart = height - 1;
+		yEnd = 0;
+		dy = -1;
+	}
+	
 	pPixels = new Color[width * height];
 
 	file.seekg(bmFileHeader.bfOffBits);
 	const int padding = (4 - (width * 3) % 4) % 4;
 
-	for (int y = height - 1; y >=0; y--)
+
+
+	for (int y = yStart; y != yEnd ; y += dy)
 	{
 		for (int x = 0; x < width; x++)
 		{
-			putPixel(x, y, Color( file.get(), file.get(), file.get() ));
+			putPixel(x, yStart - y , Color( file.get(), file.get(), file.get() ));
+			if (bmInfoHeader.biBitCount == 32) { file.seekg(1, std::ios::cur); } //add logic for transparency pixel
 		}
 		file.seekg(padding, std::ios::cur);
 	}
