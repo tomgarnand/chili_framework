c87e71d3b2348cc44df8a243a6546faa7f6d463b
Tom Garnand <tom.garnand@gmail.com>
2023-10-09 13:08:12 -0500

    grabbed new work

--- Engine/Entity.h
+++ Engine/Entity.h
@@ -1,29 +1,33 @@
 #pragma once
 
 #include <vector>
+#include <map>
+#include <unordered_map>
 #include "Vec2.h"
 #include "Drawable.h"
+#include "Action.h"
+#include "Animation.h"
+#include "Action.h"
+
 
 class Entity
 {
 public:
-	Entity( Surface& src ,const Vec2& pos = { 0.0f,0.0f })
+	Entity( Surface& src )
 		:
-		pos( pos ),
-		src( src )
-	{}
-	const Vec2& GetPos() const
+		src( src ),
+		current_action(Idle)
 	{
-		return pos;
 	}
-	void SetPos( const Vec2& newPos )
+	const Vec2& GetPos(const std::string& map) const
 	{
-		pos = newPos;
+		return pos.at(map);
 	}
-	void TranslateBy( const Vec2& offset )
+	void SetPos(const std::string& map, const Vec2& newPos )
 	{
-		pos += offset;
+		pos[map] = newPos;
 	}
+	//void TranslateBy( const Vec2& offset ){pos += offset;}
 	void SetScale( float s )
 	{
 		scale = s;
@@ -41,19 +45,52 @@ public:
 		return angle;
 	}
 
-	Drawable GetDrawable() const
+	Drawable GetDrawable(const std::string& map) const
 	{
 		Drawable d( src );
 		d.ApplyTransformation(
-			Mat3::Translation( pos.x,pos.y ) *
+			Mat3::Translation( pos.at(map).x,pos.at(map).y ) *
 			Mat3::Scale( scale ) *
 			Mat3::Rotation( angle )
 		);
 		return d;
 	}
+
+	int GetArmorClass() const { return ArmorClass; }
+	const Attributes& GetStats() const { return stats; }
+
+	void EndTick(std::vector<std::string>& stateStack);
+	void StartTick(std::vector<std::string>& stateStack);
+	void StartAction(const Action& action, const std::vector<Entity&> targets_in);
+	bool IsActionEnded();
+	void Apply(const Application& app, const Outcome& out);
+
+	void Update(std::string current_map, const Entity& player);
+
 private:
 	float angle = 0.0f;
 	float scale = 1.0f;
-	Vec2 pos = {0.0f,0.0f};
+	std::unordered_map<std::string, Vec2> pos = {0.0f,0.0f}; //convert to a map point vector
 	Surface& src;
+
+
+	//Behaviour& script;
+	Action& current_action;
+	std::vector<Entity&> targets;
+	std::vector<Action&> past_actions;
+	std::vector<Action&> action_pool;
+	int tick;
+
+
+
+	std::unordered_map<Action&, Animation&> animations; //player_animations[Slash] = Slash_Animation;
+
+	Attributes stats;
+	Status statuses;
+	float Health;
+	int ArmorClass;
+
+	//Trigger triggerType
+	bool trigger_check;
+	std::string scenario_keyword;
 };
